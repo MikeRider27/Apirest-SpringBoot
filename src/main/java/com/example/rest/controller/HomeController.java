42903128868e8917d5962b5c3ef1af6e09e7d325
Miguel Villalba <mike.mavc27@gmail.com>
2023-11-30 15:34:57 -0300

    html view controller

---
+++ src/main/java/com/example/rest/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    public String index() {
+        return "index";
+    }
+    
+}
+
